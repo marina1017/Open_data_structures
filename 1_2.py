e8547fb105e30fa5c68bc4a6466f7f2be5042416
marina1017 <mari10172001@yahoo.co.jp>
2019-06-23 21:56:57 +0900

    修正

--- 1_2.py
+++ 1_2.py
@@ -4,14 +4,17 @@
 f = open('text.txt', 'r')
 line = f.readline()
 stack = []
-count = 0
-count1 = 0
+def reverse_stack_print(stack):
+    for _ in range(len(stack)):
+            print("stack.pop()",stack.pop())
+
 
 while line:
     stack.append(line.strip())
     if len(stack) == 3:
-        for _ in range(len(stack)):
-            print("stack.pop()",stack.pop())
+        reverse_stack_print(stack)
     line = f.readline()
-    
+
+reverse_stack_print(stack)
+
 f.close()
