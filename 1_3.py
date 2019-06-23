ec2bfa6ae633a8a68600e06b09016bf84ae043b5
marina1017 <mari10172001@yahoo.co.jp>
2019-06-23 22:04:52 +0900

    修正

--- 1_3.py
+++ 1_3.py
@@ -17,16 +17,16 @@ f = open('text.txt', 'r')
 line = f.readline()
 stack = []
 
+def reverse_print(stack):
+    for _ in range(len(stack)):
+            print("stack.pop()",stack.pop())
+
 while line:
     stack.append(line.strip())
-    if len(stack) < 3 and line == '\n':
+    if len(stack) >= 3 and line == '\n':
         print("3行目以降で改行ありました")
         stack.pop()
-        for _ in range(len(stack)):
-            print("stack.pop()",stack.pop())
-    else:
-        if len(stack) == 3:
-            for _ in range(len(stack)):
-                print("stack.pop()",stack.pop())
+        reverse_print(stack)
     line = f.readline()
+reverse_print(stack)
 f.close()
