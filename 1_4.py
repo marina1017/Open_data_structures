3dfdcc62d77314e0a6140f72140a22eb8c93f4f4
marina1017 <mari10172001@yahoo.co.jp>
2019-06-23 22:27:42 +0900

    修正した

--- 1_4.py
+++ 1_4.py
@@ -2,15 +2,16 @@
 # ファイルの読み込み
 # ----------------------------------------------------
 f = open('text.txt', 'r')
-line = f.readline()
+line = f.readline().strip()
 stack = []
 set = set()
 
 while line:
+    
     if line in set:
         pass
     else:
-        set.add(line.strip())
+        set.add(line)
         print(line)
     line = f.readline()
 
