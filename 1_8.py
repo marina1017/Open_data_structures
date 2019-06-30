1d86b6466a53cf3a78e92d4fc95dda7322cd9473
marina1017 <mari10172001@yahoo.co.jp>
2019-06-30 21:39:27 +0900

    修正

--- 1_8.py
+++ 1_8.py
@@ -8,16 +8,21 @@ test_data = open("text1.txt", "r")
 # 行ごとにすべて読み込んでリストデータにする
 lines = test_data.readlines()
 striped_lines = map(lambda x:x.strip(),lines)
-lists = []
+queue_list1 = []
+queue_list2 = []
+
 #行数を数える
 count = 0
 for line in striped_lines:
     #偶数ならば
     if count % 2 == 0:
-        print(line)
+        queue_list1.append(line)
     else:
-        lists.append(line)
+        queue_list2.append(line)
     count += 1
 
-for list in lists:
+for list in queue_list1:
+    print(list)
+
+for list in queue_list2:
     print(list)
